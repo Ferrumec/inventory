41fd7c054c420fc254d9cd04f5c32329658cad3a
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-05-16 11:54:00 +0300

    introduced actixutils, updated features

--- src/config.rs
+++ src/config.rs
@@ -4,8 +4,8 @@ use crate::handlers;
 use crate::services::InventoryService;
 use crate::subscriptions::subscribe;
 use actix_web::web;
-use auth_middleware::Auth;
-use auth_middleware::Validate;
+use actixutils::OrphanWrapper;
+use actixutils::{Identity, Validate};
 use event_stream::EventStream;
 use sqlx::Error;
 use sqlx::Pool;
@@ -14,27 +14,27 @@ use sqlx::Sqlite;
 #[derive(Clone)]
 pub struct InventoryModule {
     pub service: Arc<InventoryService>,
-    validator: Arc<dyn Validate>,
+    validator: Arc<dyn Validate<Identity>>,
 }
 
 impl InventoryModule {
     pub async fn new(
-        es: Arc<dyn EventStream>,
+        es: event_stream::OrphanWrapper<Arc<dyn EventStream>>,
         pool: Pool<Sqlite>,
-        validator: Arc<dyn Validate>,
+        validator: OrphanWrapper<Arc<dyn Validate<Identity>>>,
     ) -> Result<Self, Error> {
-        let service = Arc::new(InventoryService::new(pool.clone(), es.clone())?);
-        subscribe(es, service.clone()).await;
-        Ok(Self { service, validator })
+        let service = Arc::new(InventoryService::new(pool.clone(), es.0.clone())?);
+        subscribe(es.0, service.clone()).await;
+        Ok(Self {
+            service,
+            validator: validator.0,
+        })
     }
 
     pub fn config(&self, cfg: &mut web::ServiceConfig, namespace: &str) {
         cfg.service(
             web::scope(namespace)
                 .app_data(self.service.clone())
-                .wrap(Auth {
-                    validator: self.validator.clone(),
-                })
                 .route("/items", web::get().to(handlers::list_items))
                 .route("/items", web::post().to(handlers::create_item))
                 .route(
